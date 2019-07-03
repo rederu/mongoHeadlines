6d6f2ca844e0dca1d19c4b0912f2dc7dd60534b6
Mayra Cuenca <mayra.cuencac@gmail.com>
2019-07-03 00:35:52 -0500

    Created server and models' prototypes, added new folders. Added dependencies to package.json

---
+++ server.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+// Initialize Express
+var app = express();
+var PORT = 3000;
+
+// Configure middleware
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+
+// Routes
+require("./routes/routes")(app);
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/siliconeraNews", { useNewUrlParser: true });
+
+// Start the server
+app.listen(PORT, function () {
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+    );
+});
